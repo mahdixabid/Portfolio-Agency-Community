9f341e542d6cff0302f4094ba271bd146ec99454
yuri.exe <77060124+mahdixabid@users.noreply.github.com>
2022-06-30 09:28:15 +0100

    update

--- script.js
+++ script.js
@@ -12,3 +12,21 @@ let card = document.querySelector('.card')
 card.addEventListener('mouseout', function() {
     card.classList.add('card-out');
 })
+
+
+/* slider */
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
